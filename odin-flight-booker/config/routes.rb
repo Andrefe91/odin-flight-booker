9c734b8a58aa7939c90e7eac76172729315f9aa0
Joshua <andrefe91@gmail.com>
2024-02-22 09:47:44 -0500

    Passenger along with Booking model, controller and views. Migration done

--- odin-flight-booker/config/routes.rb
+++ odin-flight-booker/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'passengers/index'
+  get 'passengers/new'
+  get 'passengers/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
