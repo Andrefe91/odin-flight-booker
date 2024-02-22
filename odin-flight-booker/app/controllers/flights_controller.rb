5f2308d99f99fcbc5535d928bd78e52e91611a5c
Andres Munoz <andrefe91@gmail.com>
2024-02-21 23:22:59 -0500

    View update and controller query for flights

--- odin-flight-booker/app/controllers/flights_controller.rb
+++ odin-flight-booker/app/controllers/flights_controller.rb
@@ -1,11 +1,19 @@
 class FlightsController < ApplicationController
   def index
     @airports_options = Airport.all.map {|airport| [airport.name_code, airport.id]}
-    @flight_dates = Flight.all.map {|flight| [flight.date, flight.id]}.sort.uniq(&:first)
+    @flight_dates = Flight.all.map {|flight| [flight.date, flight.date]}.sort.uniq(&:first)
 
     @selected_departure_airport_id = params[:departure_airport_id]
     @selected_arrival_airport_id = params[:arrival_airport_id]
     @selected_passangers_number = params[:passangers_number]
-    @selected_date = params[:flight_id]
+    @selected_date = params[:date]
+
+    @flight_options_sql = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND date > ?",
+    params[:departure_airport_id], params[:arrival_airport_id], params[:date]).order(:date)
+
+  end
+
+  def new
+    
   end
 end
--- odin-flight-booker/app/models/flight.rb
+++ odin-flight-booker/app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def flight_text
+    "Flight number: #{id}, Duration: #{duration} minutes with date: #{date}"
+  end
 end
